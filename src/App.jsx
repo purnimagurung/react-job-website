d7769ef065787711af401059d44454013b996618
purnimagurung <grg.puru@gmail.com>
2024-07-16 17:25:51 +0200

    added card features, text-slicing , added json files and fetch the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import JobCard from './components/JobCard'
+import JobListing from './components/JobListing'
+import ViewJobs from './components/ViewJobs'
 
 const App = () => {
   return (
-    <div>App</div>
+   <>
+   <Navbar />
+   <Banner />
+   <JobCard />
+   <JobListing />
+   <ViewJobs />
+   </>
   )
 }
 
---
+++ src/components/JobListing.jsx
@@ -0,0 +1,25 @@
+import JobList from './JobList';
+import jobs from '../jobs.json';
+
+const JobListing = ( {title = 'Browser Jobs'}) => {
+  // Display only three jobs vaccancy
+  const recentJobs = jobs.slice(0,3);
+  return (
+    <>
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+            <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            {title}
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {recentJobs.map((job)=> (
+              <JobList key = {job.id} job={job} />
+             ))}
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default JobListing
