b75bb683058b5e119943af679700b8dc90e778b0
purnimagurung <grg.puru@gmail.com>
2024-07-22 22:03:44 +0200

    use dataloader and create AddJobPage

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-import JobPage from './pages/JobPage';
+import JobPage, {jobLoader} from './pages/JobPage';
+import AddJobpage from './pages/AddJobpage';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element= {<MainLayout/>}>
       <Route index element= {<HomePage/>} />
       <Route path = '/jobs' element= {<JobsPage/>} />
+      <Route path = 'jobs/:id' element= {<JobPage/>} loader = {jobLoader} />
+      <Route path = '/add-job' element= {<AddJobpage/>} />
       <Route path = '*' element= {<NotFoundPage/>} />
-      <Route path = 'jobs/:id' element= {<JobPage/>} />
     </Route>
   )
 );
---
+++ src/pages/AddJobpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddJobpage = () => {
+  return (
+    <div>AddJobpage</div>
+  )
+}
+
+export default AddJobpage
