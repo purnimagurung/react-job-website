de311ab1a6f046d5d81f4962f1cdb53c337103a2
purnimagurung <grg.puru@gmail.com>
2024-07-24 17:43:22 +0200

    edit job page/form , update form submission , PUT request to update job

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,13 @@ import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobpage from './pages/AddJobpage';
 import { addJob } from './Crud/AddNewJob';
 import { deleteJob } from './Crud/DeleteJob';
+import { updateJob } from './Crud/updateJob';
+import EditJobPage from './pages/EditJobPage';
 
 
 
 
 const App = () => {
-
-/*   post request to add job */
-
-  // const addJob = async (newJob) => {
-  //   const res = await fetch ('/api/jobs', {
-  //     method : 'POST',
-  //     headers : {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body:JSON.stringify(newJob)
-  //   });
-  //   return;
-  // }
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element= {<MainLayout/>}>
@@ -33,6 +22,7 @@ const App = () => {
         <Route path = '/jobs' element= {<JobsPage/>} />
         <Route path = 'jobs/:id' element= {<JobPage  deleteJob = {deleteJob} />}  loader = {jobLoader} />
         <Route path = '/add-job' element= {<AddJobpage addJobSubmit={addJob}/>} />
+        <Route path = '/edit-job/:id' element= {<EditJobPage updateJobSubmit = {updateJob} />}  loader = {jobLoader} />
         <Route path = '*' element= {<NotFoundPage/>} />
       </Route>
     )
---
+++ src/Crud/updateJob.jsx
@@ -0,0 +1,10 @@
+export const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return res.json();
+  };
