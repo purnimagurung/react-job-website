95b53beed054b5083ca1e75a38056a202abc97cf
purnimagurung <grg.puru@gmail.com>
2024-07-24 16:22:13 +0200

    DELETE request to remove job

--- src/Crud/DeleteJob.jsx
+++ src/Crud/DeleteJob.jsx
@@ -1,11 +1,6 @@
 export const deleteJob = async (id) => {
-    console.log('delete', id);
-    // const res = await fetch('/api/jobs', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(newJob)
-    // });
-    // return res.json();
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
   };
