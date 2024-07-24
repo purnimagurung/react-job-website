818aa57c46f609027fc13f6a8cad979754cc1f22
purnimagurung <grg.puru@gmail.com>
2024-07-24 12:57:35 +0200

    form submission

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -14,7 +14,7 @@ const JobCard = () => {
                 Browse our React jobs and start your career today
               </p>
               <Link
-                to="/jobs.html"
+                to="/jobs"
                 className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
               >
                 Browse Jobs
